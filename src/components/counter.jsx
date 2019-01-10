13fdd68ffd940bdd9c674e3b12a02fb63601f23e
Jordan Pearson <jspearson@pugetsound.edu>
2019-01-09 18:57:36 -0800

    Add badge coloring method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+
   render() {
-    React.createElement("div");
     return (
       <div>
-        <h1>Shop</h1>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses}>{this.formatCount()}</span>
+        <button className="btn btn-secondary">Increment</button>
       </div>
     );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
