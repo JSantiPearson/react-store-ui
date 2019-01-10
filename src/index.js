993850d70d0c598ff0586585b9cc598e1749ee1b
Jordan Pearson <jspearson@pugetsound.edu>
2019-01-09 18:07:01 -0800

    add button for incrementing

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter.jsx";
+import Counter from "./components/counter";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root"));
 
 serviceWorker.register();
